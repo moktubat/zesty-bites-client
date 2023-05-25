42739f96faf866ddcd5493096f6381c664c8c3f0
moktubat <shadhinreja@gmail.com>
2023-05-25 23:40:31 +0600

    testimonials added

---
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -0,0 +1,43 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import qouteImg from '../../../assets/quote.png'
+
+import "@smastrom/react-rating/style.css";
+
+const Testimonial = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <div className="my-16">
+      <SectionTitle
+        subHeading={"What Our Clients Say"}
+        heading={"TESTIMONIALS"}
+      ></SectionTitle>
+      <div>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="flex flex-col items-center mx-24 my-16">
+                <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+                <img className="w-28 pt-4" src={qouteImg} alt="" />
+                <p className="py-8">{review.details}</p>
+                <h3 className="text-2xl text-[#CD9003]">{review.name}</h3>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
